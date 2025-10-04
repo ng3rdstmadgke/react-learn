d811a66c90855e12226c5a7ef2999d1430bb9a35
ktamido <ng3rdstmadgke@gmail.com>
2025-10-04 14:27:07 +0900

    first

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-DOCKER_NETWORK=br-lang-template-${USER}
+DOCKER_NETWORK=br-react-learn-${USER}
 NETWORK_EXISTS=$(docker network ls --filter name=$DOCKER_NETWORK --format '{{.Name}}')
 
 if [ -z "$NETWORK_EXISTS" ]; then
